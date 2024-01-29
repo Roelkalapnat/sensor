6665b97b5f60d4d14235e9f35e1ef701e9f9dd04
Roelkalapnat <152483260+Roelkalapnat@users.noreply.github.com>
2024-01-29 14:44:57 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    if (maqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14) < 10) {
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Backward, 100)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Backward, 0)
+        basic.pause(500)
+    } else {
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
+    }
+})
